e8db0d14f3105be10ffdf7ac304237405c886cc0
Alfred Sollie Rønning <alfredronning@gmail.com>
2023-12-17 22:44:23 +0100

    day17

---
+++ 2023/day17/solver2.py
@@ -0,0 +1,30 @@
+import heapq
+
+def search(board, start, end):
+    visited = dict()
+    queue = [(0, start, (0, 0), 0)]
+    while queue:
+        heatloss, current, dir, dir_count = heapq.heappop(queue)
+        if (current, dir, dir_count) in visited:
+            continue
+        visited[(current, dir, dir_count)] = heatloss 
+        if current == end:
+            if dir_count >= 4:
+                return heatloss
+            continue
+        for d in [(1, 0), (0, 1), (0, -1), (-1, 0)]:
+            if (-d[0], -d[1]) == dir:
+                continue
+            neighbour_dir_count = 1 if d != dir else dir_count + 1
+            if neighbour_dir_count > 10:
+                continue
+            if neighbour_dir_count == 1 and dir_count < 4 and current != start:
+                continue
+            neighbour = current[0]+d[0], current[1]+d[1]
+            if 0 <= neighbour[0] <= len(board) and 0 <= neighbour[1]>=0 and neighbour[0]<len(board) and neighbour[1]<len(board[0]):
+                neighbour_heatloss = heatloss + board[neighbour[0]][neighbour[1]]
+                heapq.heappush(queue, (neighbour_heatloss, neighbour, d, neighbour_dir_count)) # type: ignore
+
+if __name__ == "__main__":
+    board = [[int(i) for i in row] for row in open("input.txt").read().strip().split("\n")]
+    print(search(board, (0, 0), ((len(board[0])-1, len(board[0])-1))))
